ac0265dcdd0c8df280b11af00262d96704c44e33
gabo0527 <gabriel@manocg.com>
2025-11-20 10:32:57 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FinancialCalculatorApp from './financial-calculator.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FinancialCalculatorApp />
-  </React.StrictMode>,
-)
